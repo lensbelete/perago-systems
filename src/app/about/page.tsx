7d477ccf63f03b354977c1e3a08487cd01e84cba
mariatesfaye <mariatesfaye67@gmail.com>
2024-05-25 20:34:10 +0300

    added about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,25 +3,38 @@ import peragoLogo from "../../assets/perago1.png";
 
 const AboutUs = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-start pt-8">
-      <h1 className="text-4xl font-bold text-green-600 mb-8">About Us</h1>
+    <div className="min-h-screen flex flex-col items-center justify-start pt-8 bg-gray-100">
+      <h1 className="text-4xl d text-green-600 mb-8">About Us</h1>
       <div className="w-full flex justify-center mb-8">
         <Image src={peragoLogo} alt="Perago logo" />
       </div>
-      <div className="max-w-4xl px-4 text-center">
-        <p className="text-lg leading-relaxed mb-4">
-          Welcome to Perago! We are dedicated to providing innovative solutions
-          for your business needs. Our team is committed to excellence and
-          delivering value through our services. Thank you for choosing Perago.
+      <div className="px-14">
+        <h2 className="text-3 text-green-600 mb-5">Who We Are</h2>
+        <hr className="border-gray-300 w-full mb-6" />
+        <p className="text-gray-600 text-sm leading-relaxed mb-4">
+          <span className="text-gray-600 font-bold">
+            Perago Information Systems PLC
+          </span>{" "}
+          is a cutting-edge and innovative technology-based solution providing
+          company with a focus on designing and developing multi-channel based
+          software solutions using Web, SMS, WAP, IVR, Kiosk, social media, and
+          mobile applications; custom software development and quality assurance
+          services; E-services consulting, development, and implementation;
+          design innovative public service delivery initiatives using technology
+          solutions; research and training services; E-governance enablement;
+          ICT consulting; and capacity building, in Ethiopia and beyond.
         </p>
-        <p className="text-lg leading-relaxed mb-4">
-          Our mission is to empower businesses with the tools they need to
-          succeed in today's competitive market. We strive to build long-lasting
-          relationships with our clients based on trust and mutual respect.
-        </p>
-        <p className="text-lg leading-relaxed">
-          Contact us today to learn more about how we can help your business
-          grow and achieve its goals.
+        <p className="text-gray-600 text-sm leading-relaxed mb-8">
+          Perago is established by highly qualified and experienced
+          professionals in management, economics, business administration,
+          software engineering, and information systems management; and it also
+          houses professionals who are internationally certified in leading-edge
+          information and communication technologies. Perago as well provides
+          strong technical support and knowledge transfer to sustain the
+          competitive advantages gained by customers from their investments for
+          Perago’s services. Beside the technical capacity, Perago’s management
+          has commendable entrepreneurial flair with strong ethical and
+          professional standards.
         </p>
       </div>
     </div>
