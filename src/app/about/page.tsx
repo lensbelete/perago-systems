15d5486df450da412048d94669682ac09072dce6
mariatesfaye <mariatesfaye67@gmail.com>
2024-05-27 18:00:36 +0300

    edit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,21 +4,41 @@ import { Flex, Title } from "@mantine/core";
 
 const AboutUs = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-start">
-      <Title order={1} mb="sm" style={{ color: "green" }} className="pt-6">
+    <div className="min-h-screen flex flex-col items-center justify-start container mx-auto">
+      <Title
+        order={1}
+        mb="sm"
+        style={{ color: "#55ba4f", fontWeight: "normal" }}
+        className="pt-6"
+      >
         About Us
       </Title>
 
       <div className="w-full flex justify-center mb-1">
-        <Image src={peragoLogo} alt="perago logo for about page" />
+        <Image
+          src={peragoLogo}
+          className="w-screen"
+          alt="perago logo for about page"
+        />
       </div>
-      <div className="px-14  bg-gray-100">
-        <Title order={3} mb="sm" style={{ color: "green" }} className="pt-6">
+      <div className="px-14 bg-gray-100">
+        <Title
+          order={3}
+          mb="sm"
+          style={{ color: "#55ba4f", fontWeight: "normal", fontSize: "1.2rem" }}
+          className="pt-6"
+        >
           Who We Are
         </Title>
         <hr className="border-gray-300 w-full mb-6" />
-        <p className="text-gray-600  leading-relaxed mb-4 text-lg">
-          <span className="text-gray-600 font-bold">
+        <p
+          className="leading-relaxed mb-4 text-lg"
+          style={{ color: "#343a40", fontSize: "0.9rem" }}
+        >
+          <span
+            className="font-bold"
+            style={{ color: "#343a40", fontSize: "1.0rem" }}
+          >
             Perago Information Systems PLC
           </span>{" "}
           is a cutting-edge and innovative technology-based solution providing
@@ -30,7 +50,10 @@ const AboutUs = () => {
           solutions; research and training services; E-governance enablement;
           ICT consulting; and capacity building, in Ethiopia and beyond.
         </p>
-        <p className="text-gray-600  leading-relaxed mb-8 text-lg">
+        <p
+          className="leading-relaxed mb-8 text-lg"
+          style={{ color: "#343a40", fontSize: "0.9rem" }}
+        >
           Perago is established by highly qualified and experienced
           professionals in management, economics, business administration,
           software engineering, and information systems management; and it also
@@ -44,11 +67,11 @@ const AboutUs = () => {
         </p>
       </div>
 
-      <div className="w-full bg-white py-14">
+      <div className="w-full bg-white py-20">
         <Title
           order={1}
           mb="sm"
-          style={{ color: "green" }}
+          style={{ color: "#55ba4f", fontWeight: "normal", fontSize: "2.6rem" }}
           className="pt-6 text-center"
         >
           Why Perago?
@@ -57,10 +80,9 @@ const AboutUs = () => {
         <Title
           order={1}
           mb="sm"
-          style={{ color: "green" }}
-          className="pt-6 text-center"
+          style={{ color: "#55ba4f", fontWeight: "normal", fontSize: "2.0rem" }}
+          className="pt-1 text-center"
         >
-          {" "}
           Reasons to choose us
         </Title>
 
@@ -74,14 +96,23 @@ const AboutUs = () => {
             <Title
               order={4}
               mb="sm"
-              style={{ color: "green" }}
+              style={{
+                color: "#55ba4f",
+                fontWeight: "normal",
+                fontSize: "1.0rem",
+              }}
               className="pt-6"
             >
               Diversified Experience in Software Engineering
             </Title>
 
             <hr className="border-gray-300 mb-4" />
-            <p className="text-gray-600 leading-relaxed text-lg">
+            <p
+              className="text-gray-600 leading-relaxed text-lg"
+              style={{
+                fontSize: "1.0rem",
+              }}
+            >
               We have hands on consulting and implementation experiences in
               software development, system analysis, and software quality
               assurance at national and regional levels.
@@ -91,14 +122,23 @@ const AboutUs = () => {
             <Title
               order={4}
               mb="sm"
-              style={{ color: "green" }}
+              style={{
+                color: "#55ba4f",
+                fontWeight: "normal",
+                fontSize: "1.0rem",
+              }}
               className="pt-6"
             >
               Qualified Staff
             </Title>
 
             <hr className="border-gray-300 mb-4" />
-            <p className="text-gray-600 text-lg leading-relaxed">
+            <p
+              className="text-gray-600 text-lg leading-relaxed"
+              style={{
+                fontSize: "1.0rem",
+              }}
+            >
               Perago houses internationally certified professionals among the
               few ones in the industry. This highly dedicated and committed
               staff are the cornerstones for providing quality products and
@@ -111,14 +151,23 @@ const AboutUs = () => {
             <Title
               order={4}
               mb="sm"
-              style={{ color: "green" }}
+              style={{
+                color: "#55ba4f",
+                fontWeight: "normal",
+                fontSize: "1.0rem",
+              }}
               className="pt-6"
             >
               Customer Focus
             </Title>
 
             <hr className="border-gray-300 mb-4" />
-            <p className="text-gray-600 text-lg leading-relaxed">
+            <p
+              className="text-gray-600 text-lg leading-relaxed"
+              style={{
+                fontSize: "1.0rem",
+              }}
+            >
               We foster close relationships, rooted in trust and respect, with
               our customers, and face together the everyday challenge of tying
               the evolving information technology with their business goals.
@@ -136,14 +185,23 @@ const AboutUs = () => {
             <Title
               order={4}
               mb="sm"
-              style={{ color: "green" }}
+              style={{
+                color: "#55ba4f",
+                fontWeight: "normal",
+                fontSize: "1.0rem",
+              }}
               className="pt-6"
             >
               Understand Customers’ needs
             </Title>
 
             <hr className="border-gray-300 mb-4" />
-            <p className="text-gray-600 text-lg leading-relaxed">
+            <p
+              className="text-gray-600 text-lg leading-relaxed"
+              style={{
+                fontSize: "1.0rem",
+              }}
+            >
               We strive to understand our customers’ business, user,
               operational, and system requirements before designing solutions.
               Perago is very agile in addressing business process changes during
@@ -154,15 +212,24 @@ const AboutUs = () => {
             <Title
               order={4}
               mb="sm"
-              style={{ color: "green" }}
+              style={{
+                color: "#55ba4f",
+                fontWeight: "normal",
+                fontSize: "1.0rem",
+              }}
               className="pt-6"
             >
               Awareness of all stakeholders
             </Title>
 
             <hr className="border-gray-300 mb-4" />
-            <p className="text-gray-600 text-lg leading-relaxed">
-              we engage all stakeholders in every phase of our project life
+            <p
+              className="text-gray-600 text-lg leading-relaxed"
+              style={{
+                fontSize: "1.0rem",
+              }}
+            >
+              We engage all stakeholders in every phase of our project life
               cycle in order to get the confidence of users and to ensure the
               solutions are put into service.
             </p>
@@ -171,82 +238,29 @@ const AboutUs = () => {
             <Title
               order={4}
               mb="sm"
-              style={{ color: "green" }}
+              style={{
+                color: "#55ba4f",
+                fontWeight: "normal",
+                fontSize: "1.0rem",
+              }}
               className="pt-6"
             >
               Minimize the Total Cost of Ownership (TCO)
             </Title>
 
             <hr className="border-gray-300 mb-4" />
-            <p className="text-gray-600 text-lg leading-relaxed">
+            <p
+              className="text-gray-600 text-lg leading-relaxed"
+              style={{
+                fontSize: "1.0rem",
+              }}
+            >
               Perago houses internationally certified professionals among the
               few ones in the industry. This highly dedicated and committed
-              staffs are the cornerstones for providing quality products and
-              services to our customers.We also engage subject matter experts to
-              make sure we understand every bit of our customers’ business
+              staff are the cornerstones for providing quality products and
+              services to our customers. We also engage subject matter experts
+              to make sure we understand every bit of our customers’ business
               processes.
-            </p>
-          </div>
-        </Flex>
-
-        <Flex
-          direction={{ base: "column", sm: "row" }}
-          gap={{ base: "sm", sm: "lg" }}
-          justify={{ sm: "center" }}
-          className="p-5"
-        >
-          <div className="flex-1 w-full md:max-w-[30%] p-6 bg-gray-50 rounded-lg shadow mx-auto">
-            <Title
-              order={4}
-              mb="sm"
-              style={{ color: "green" }}
-              className="pt-6"
-            >
-              Adaptation of technologies
-            </Title>
-
-            <hr className="border-gray-300 mb-4" />
-            <p className="text-gray-600 text-lg leading-relaxed">
-              Fast adaptation of new technologies is the core of our business
-              strategy. We help our customers embrace the benefits of technology
-              to realize their business goals.
-            </p>
-          </div>
-          <div className="flex-1 w-full md:max-w-[30%] p-6 bg-gray-50 rounded-lg shadow mx-auto">
-            <Title
-              order={4}
-              mb="sm"
-              style={{ color: "green" }}
-              className="pt-6"
-            >
-              Knowledge Transfer
-            </Title>
-
-            <hr className="border-gray-300 mb-4" />
-            <p className="text-gray-600 text-lg leading-relaxed">
-              We always work in close partnership with our customers from
-              requirement understanding to solution delivery. During all the
-              phases we make sure that we able to impart our knowledge to
-              customers.
-            </p>
-          </div>
-          <div className="flex-1 w-full md:max-w-[30%] p-6 bg-gray-50 rounded-lg shadow mx-auto">
-            <Title
-              order={4}
-              mb="sm"
-              style={{ color: "green" }}
-              className="pt-6"
-            >
-              Timely Delivery & Support after solution delivery
-            </Title>
-
-            <hr className="border-gray-300 mb-4" />
-            <p className="text-gray-600 text-lg leading-relaxed">
-              To ensure long term successes, a high standing and solid
-              reputation in the marketplace are required. It is therefore a
-              Perago standard to deliver high quality products and services
-              within budget and time. We have the commitment to provide world
-              class support service to customers’ request.
             </p>
           </div>
         </Flex>
