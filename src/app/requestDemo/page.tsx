8e8af3888b182818a091e28d1fde4cb80944a00a
mariatesfaye <mariatesfaye67@gmail.com>
2024-05-27 12:03:38 +0300

    submit

--- src/app/requestDemo/page.tsx
+++ src/app/requestDemo/page.tsx
@@ -7,14 +7,11 @@ import {
   Title,
   Group,
   Button,
-
   Box,
-
   Textarea,
   Card,
 } from "@mantine/core";
 import { useRouter } from "next/navigation";
-
 
 const RequestDemo = () => {
   const form = useForm({
@@ -43,155 +40,140 @@ const RequestDemo = () => {
   });
   const router = useRouter();
 
-
   const handleSubmit = async (values: any) => {
-    
     console.log(values);
-    router.push('/submit');
+    router.push("/submit");
   };
 
   return (
-    <Container size="md"  className="h-screen">
+    <Container size="md" className="h-screen">
+      <Card
+        shadow="sm"
+        withBorder
+        style={{ backgroundColor: "#f0f0f0" }}
+        className="p-32 pt-3 h-screen "
+      >
         <Card
           shadow="sm"
+          className="p-10 mt-1 overflow-y-scroll"
+          radius="md"
           withBorder
-          style={{ backgroundColor: "#f0f0f0"}}
-          className="p-32 pt-3 h-screen "
         >
-          <Card shadow="sm" className="p-10 mt-1 overflow-y-scroll" radius="md" withBorder>
-            <Title  mb="xl" className="text-center">
-              Request Demo
-            </Title>
-            <form onSubmit={form.onSubmit(handleSubmit)}>
-              <Group  mt="lg" mb="xl">
-                
-                    <TextInput
-                      withAsterisk
-                      label="Full Name"
-                      placeholder="Full Name"
-                      {...form.getInputProps("fullName")}
-                      styles={{
-                        input: {
-                          backgroundColor: form.errors.fullName
-                            ? "#ffe4e4"
-                            : "transparent",
-                          width: "300px",
-                        },
-                      }}
-                    />
-                 
-                  
-                 
-              </Group>
+          <Title mb="xl" className="text-center">
+            Request Demo
+          </Title>
+          <form onSubmit={form.onSubmit(handleSubmit)}>
+            <Group mt="lg" mb="xl">
+              <TextInput
+                withAsterisk
+                label="Full Name"
+                placeholder="Full Name"
+                {...form.getInputProps("fullName")}
+                styles={{
+                  input: {
+                    backgroundColor: form.errors.fullName
+                      ? "#ffe4e4"
+                      : "transparent",
+                    width: "300px",
+                  },
+                }}
+              />
+            </Group>
 
-              <Group mt="lg" mb="xl">
-               
-                  
-                    <TextInput
-                      withAsterisk
-                      label="Company Name"
-                      placeholder="Company Name"
-                      {...form.getInputProps("companyName")}
-                      styles={{
-                        input: {
-                          backgroundColor: form.errors.companyName
-                            ? "#ffe4e4"
-                            : "transparent",
-                          width: "300px",
-                        },
-                      }}
-                    /> 
-              </Group>
+            <Group mt="lg" mb="xl">
+              <TextInput
+                withAsterisk
+                label="Company Name"
+                placeholder="Company Name"
+                {...form.getInputProps("companyName")}
+                styles={{
+                  input: {
+                    backgroundColor: form.errors.companyName
+                      ? "#ffe4e4"
+                      : "transparent",
+                    width: "300px",
+                  },
+                }}
+              />
+            </Group>
 
-              <Group  mt="lg" mb="xl">
-                    <TextInput
-                      withAsterisk
-                      label="Email"
-                      placeholder="Email"
-                      {...form.getInputProps("email")}
-                      styles={{
-                        input: {
-                          backgroundColor: form.errors.email
-                            ? "#ffe4e4"
-                            : "transparent",
-                          width: "300px",
-                        },
-                      }}
-                    />
-                       
-              </Group>
+            <Group mt="lg" mb="xl">
+              <TextInput
+                withAsterisk
+                label="Email"
+                placeholder="Email"
+                {...form.getInputProps("email")}
+                styles={{
+                  input: {
+                    backgroundColor: form.errors.email
+                      ? "#ffe4e4"
+                      : "transparent",
+                    width: "300px",
+                  },
+                }}
+              />
+            </Group>
 
-              <Group
-                mt="lg"
-                mb="xl"
-                style={{ alignItems: "flex-end" }}
-              >
-                    <TextInput
-                      withAsterisk
-                      label="Phone Number"
-                      placeholder="Area Code"
-                      {...form.getInputProps("areaCode")}
-                      styles={{
-                        input: {
-                          backgroundColor: form.errors.areaCode
-                            ? "#ffe4e4"
-                            : "transparent",
-                          width: "200px",
-                        },
-                      }}
-                    />
-                  
-          
-                <TextInput
-                  withAsterisk
-                  label=""
-                  placeholder="Phone Number"
-                  {...form.getInputProps("phoneNumber")}
-                  styles={{
-                    input: {
-                      backgroundColor: form.errors.phoneNumber
-                        ? "#ffe4e4"
-                        : "transparent",
-                      width: "200px",
-                    },
-                  }}
-                />
-              </Group>
+            <Group mt="lg" mb="xl" style={{ alignItems: "flex-end" }}>
+              <TextInput
+                withAsterisk
+                label="Phone Number"
+                placeholder="Area Code"
+                {...form.getInputProps("areaCode")}
+                styles={{
+                  input: {
+                    backgroundColor: form.errors.areaCode
+                      ? "#ffe4e4"
+                      : "transparent",
+                    width: "200px",
+                  },
+                }}
+              />
 
-              <Group mt="lg" mb="xl">            
-                    <Textarea
-                      withAsterisk
-                      resize="both"
-                      label="How Can We Help You?"
-                      placeholder="Enter your message here"
-                      {...form.getInputProps("helpMessage")}
-                      styles={{
-                        input: {
-                          backgroundColor: form.errors.helpMessage
-                            ? "#ffe4e4"
-                            : "transparent",
-                          width: "500px",
-                          height: "100px",
-                        },
-                      }}
-                    />
-              </Group>
+              <TextInput
+                withAsterisk
+                label=""
+                placeholder="Phone Number"
+                {...form.getInputProps("phoneNumber")}
+                styles={{
+                  input: {
+                    backgroundColor: form.errors.phoneNumber
+                      ? "#ffe4e4"
+                      : "transparent",
+                    width: "200px",
+                  },
+                }}
+              />
+            </Group>
 
-              <Box mt="xl">
-                  <Button
-                    color="green"
-                    type="submit"
-                  >
-                    Submit
-                  </Button>
-              </Box>
-            </form>
-          </Card>
-          
+            <Group mt="lg" mb="xl">
+              <Textarea
+                withAsterisk
+                resize="both"
+                label="How Can We Help You?"
+                placeholder="Enter your message here"
+                {...form.getInputProps("helpMessage")}
+                styles={{
+                  input: {
+                    backgroundColor: form.errors.helpMessage
+                      ? "#ffe4e4"
+                      : "transparent",
+                    width: "500px",
+                    height: "100px",
+                  },
+                }}
+              />
+            </Group>
+
+            <Box mt="xl">
+              <Button color="green" type="submit">
+                Submit
+              </Button>
+            </Box>
+          </form>
         </Card>
-        
-      </Container>
-    
+      </Card>
+    </Container>
   );
 };
 
